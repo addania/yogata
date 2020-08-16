97606f147ab760c4274cdda37dfc88a298a41285
addania <mirka.schw@gmail.com>
2020-08-16 12:47:11 +0200

    Improving home page classes

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,16 @@ return (
   <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
     <header style={{ marginBottom: `1.5rem`, paddingTop:"20px" }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline`, fontSize:"1rem" }}>{data.site.siteMetadata.author}</h3>
+        {/*<h3 style={{ display: `inline`, fontSize:"1rem" }}>{data.site.siteMetadata.author}</h3>*/}
+        {/*<img src="https://i.imgur.com/jZrFbML.png" style={{width:"60px"}}/>*/}
+       {/* <img src="https://i.imgur.com/hCRXTmA.png" style={{width:"60px"}}/>*/}
+       {/*<img src="https://i.imgur.com/8VMTI6J.png" style={{width:"60px", color: "teal"}}/>*/}
+      {/*<img src="https://i.imgur.com/STVY8eY.png" style={{width:"50px", color: "teal"}}/>*/}
+      {/*<img src="https://i.imgur.com/YoEA22c.png" style={{width:"50px", color: "teal"}}/>*/}
+     {/*  <img src="https://i.imgur.com/gJXzd1W.png" style={{width:"50px", color: "teal"}}/>*/}
+     <img src="https://i.imgur.com/w2YLl90.png" style={{width:"50px", color: "teal"}}/>
+   
+     
       </Link>
       <ul style={{ listStyle: `none`, float: `right`}}>
         <ListLink to="/">Home</ListLink>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,82 @@
-import React from "react";
-import Layout from "../components/layout.js";
-import { Link, graphql } from "gatsby";
-import SEO from "../components/seo.js";
-import { Carousel } from "../components/carousel.js";
-import { BackgroundVideo } from "../components/video.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
+import React from "react"
+import Layout from "../components/layout.js"
+import { Link, graphql } from "gatsby"
+import SEO from "../components/seo.js"
+import { Carousel } from "../components/carousel.js"
+import { BackgroundVideo } from "../components/video.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./index.css"
+import { ReactComponent as ReactLogo } from "./logo.svg"
 
-export default ( {data} ) => {
+export default ({ data }) => {
   console.log(data)
-return (
-<div>
-{/*<BackgroundVideo />
+  return (
+    <div>
+      {/*<BackgroundVideo />
 //<div className="content">
 //<Layout className="content">*/}
- <Layout>
-  <Carousel />
-  <h3 style={{textAlign: "center"}}>SCHEDULE</h3>
-  <p style={{textAlign: "center"}}>🔥Tabata: Mondays at 18:00 CET</p>
-  <p style={{textAlign: "center"}}>🌺Yoga: Thursdays at 18:00 CET</p>
-  <h3 style={{textAlign: "center"}}>CLASS DETAILS</h3>
-  <p style={{textAlign: "center"}}>Link: https://meet.jit.si/fitwithmia</p>
-  <p style={{textAlign: "center"}}>Password: fitwithmia</p>
-  <h3 style={{textAlign: "center"}}>ALL TRAININGS</h3>
-  <p style={{textAlign: "center", fontSize:"15px", color: "grey"}}>{data.allMarkdownRemark.totalCount} Posts</p>
-  
-  <table style={{textAlign: "justify", textJustify: "inter-word"}}>
+      <Layout>
+        <Carousel />
+        <h3 style={{ textAlign: "center" }}>CLASSES</h3>
+        <div className="row" style={{ margin: "0" }}>
+          <div className="col-6" style={{ padding: "0 20px 0 0" }}>
+            <img src="https://i.imgur.com/iOw2mwe.jpg?1" />
+            <p style={{ textAlign: "center",backgroundColor: "rgba(0,0,0, 0.7)", color:"white", padding: "20px 0 20px 0" }}>
+              🔥TABATA: Mondays at 18:00 CET
+            </p>
+          </div>
+          <div className="col-6" style={{ padding: "0 0 0 20px" }}>
+            <img src="https://i.imgur.com/kSnbEOJ.jpg?1" />
+            <p style={{ textAlign: "center", backgroundColor: "rgba(0,0,0, 0.7)", color:"white", padding: "20px 0 20px 0" }}>
+              🌺YOGA: Thursdays at 18:00 CET
+            </p>
+          </div>
+
+          {/*<img className="col-6" src="https://i.imgur.com/WDUP5Gv.jpg?1" style={{}}/>*/}
+        </div>
+        <h3 style={{ textAlign: "center" }}>DETAILS</h3>
+        <p style={{ textAlign: "center" }}>
+          Link: https://meet.jit.si/fitwithmia
+        </p>
+        <p style={{ textAlign: "center" }}>Password: fitwithmia</p>
+        <h3 style={{ textAlign: "center" }}>ALL TRAININGS</h3>
+        <p style={{ textAlign: "center", fontSize: "15px", color: "grey" }}>
+          {data.allMarkdownRemark.totalCount} Posts
+        </p>
+
+        <table style={{ textAlign: "justify", textJustify: "inter-word" }}>
           <thead>
             <tr>
               <th>Date</th>
               <th>Title</th>
               {/*<th>Trainer</th>*/}
               <th>Preview</th>
-              
             </tr>
           </thead>
-          <tbody> 
+          <tbody>
             {data.allMarkdownRemark.edges.map(({ node }, index) => (
               <tr key={index}>
                 <td>{node.frontmatter.date}</td>
-                <Link to={node.fields.slug} style={{backgroundImage: "none"}}>
-                <td style={{color: "teal"}}>{node.frontmatter.title.split(" ")[0]}</td>
+                <Link to={node.fields.slug} style={{ backgroundImage: "none" }}>
+                  <td style={{ color: "teal" }}>
+                    {node.frontmatter.title.split(" ")[0]}
+                  </td>
                 </Link>
                 {/*<td>{node.frontmatter.trainer}</td>*/}
                 <td>{removeTrainerFromExcerpt(node.excerpt)}</td>
-                
               </tr>
             ))}
           </tbody>
         </table>
-
-</Layout>
-{/*</div>*/}
-</div>
-)
+      </Layout>
+      {/*</div>*/}
+    </div>
+  )
 }
 
-export const query = graphql` 
+export const query = graphql`
   query {
-    allMarkdownRemark (sort: {fields:[frontmatter___date], order:DESC}) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
@@ -78,8 +96,8 @@ export const query = graphql`
       }
       totalCount
     }
-    }
-  ` 
-  const removeTrainerFromExcerpt = (string) => {
-    return string.replace(/(with Mia )|(with Tony )/, "")
   }
+`
+const removeTrainerFromExcerpt = string => {
+  return string.replace(/(with Mia )|(with Tony )/, "")
+}
